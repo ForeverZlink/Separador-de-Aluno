877433bf73120c5dbfdc710bc9374f286e77f79d
St;ZlinkG <carloscunha080@gmail.com>
2020-11-25 19:51:10 -0200

    alterei novamente meu sistema de validação

--- modulos.py
+++ modulos.py
@@ -20,15 +20,7 @@ def leiaint(texto):
             print('NÃO É NUMERICO! DIGITE NOVAMENTE')
  
 
-def validaçao(valores,escolha):
+def validaçao(valores):
     manha={'2001':[],'2002':[],'2003':[],'2004':[]}
-    if escolha == 0 and len(manha)<=40:
-        if len(manha)<=10:
-            manha['2001'].append(valores)
-        elif len(manha)<=20:
-            manha['2002'].append(valores)
-        elif len(manha)<=30:
-            manha['2003'].append(valores)
-        else:
-            manha['2004'].append(valores)
+    print(valores)
 
--- programa principal.py
+++ programa principal.py
@@ -1,9 +1,8 @@
 from modulos import leiaint,tabela,validaçao
 
-manha={'2001':[],'2002':[],'2003':[],'2004':[]}
-tarde={'2005':[],'2006':[],'2007':[],'2008':[]}
 cont=0
 infomações={}
+tudo=[]
 while True:
     cont+=1
     infomações['aluno']=str(input('Nome do aluno:')).strip()
@@ -11,31 +10,12 @@ while True:
     infomações['data_de_nascimento']=leiaint('Data de nascimento:')
     infomações['mes_de_nascimento']=leiaint('Mês de nascimento:')
     infomações['ano_de_nascimento']=leiaint('Ano de nascimento:')
-    escolha=leiaint('Manha ou tarde(0 para manhã e 1 para de tarde):')
+    infomações['turno']=leiaint('Manha ou tarde(0 para manhã e 1 para de tarde):')
     escolha2=leiaint('1 para sair do programa 0 para continuar:')
+    tudo.append(infomações.copy())
     
-    validaçao(infomações,escolha)
-    if escolha == 0 and len(manha)<=40:
-        turno='manha'
-        if len(manha)<=10:
-            manha['2001'].append({'aluno':aluno,'matricula':matricula,'data_de_nascimento':data_de_nascimento,'mesdenascimento':mes_de_nascimento,'ano de nascimento':ano_de_nascimento,'turno':turno})
-        elif len(manha)<=20:
-            manha['2002'].append({'aluno':aluno,'matricula':matricula,'data_de_nascimento':data_de_nascimento,'mesdenascimento':mes_de_nascimento,'ano de nascimento':ano_de_nascimento,'turno':turno})
-        elif len(manha)<=30:
-            manha['2003'].append({'aluno':aluno,'matricula':matricula,'data_de_nascimento':data_de_nascimento,'mesdenascimento':mes_de_nascimento,'ano de nascimento':ano_de_nascimento,'turno':turno})
-        else:
-            manha['2004'].append({'aluno':aluno,'matricula':matricula,'data_de_nascimento':data_de_nascimento,'mesdenascimento':mes_de_nascimento,'ano de nascimento':ano_de_nascimento,'turno':turno})
-    if escolha2==0 and len(tarde)<=40:
-        turno='tarde'
-        if len(tarde)<=10:
-            tarde['2005'].append({'aluno':aluno,'matricula':matricula,'data_de_nascimento':data_de_nascimento,'mesdenascimento':mes_de_nascimento,'ano de nascimento':ano_de_nascimento,'turno':turno})
-        elif len(tarde)<=20:
-            tarde['2006'].append({'aluno':aluno,'matricula':matricula,'data_de_nascimento':data_de_nascimento,'mesdenascimento':mes_de_nascimento,'ano de nascimento':ano_de_nascimento,'turno':turno})
-        elif len(tarde)<=30:
-            tarde['2007'].append({'aluno':aluno,'matricula':matricula,'data_de_nascimento':data_de_nascimento,'mesdenascimento':mes_de_nascimento,'ano de nascimento':ano_de_nascimento,'turno':turno})
-        else:
-            tarde['2008'].append({'aluno':aluno,'matricula':matricula,'data_de_nascimento':data_de_nascimento,'mesdenascimento':mes_de_nascimento,'ano de nascimento':ano_de_nascimento,'turno':turno})
     if escolha2 == 1:
         break
 
-tabela(manha)
+
+validaçao(tudo)
