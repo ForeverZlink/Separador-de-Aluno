ef1216d9e7d23165e542a77a8648cbdcaa728844
St;ZlinkG <carloscunha080@gmail.com>
2020-11-27 23:29:49 -0200

    Criei mais funções e deixei o apresentação do progrma para o usuario final melhor. Tambem adicionei um algoritmo que checa se o usuario digitou datas validas. Agora eu tenho que adicionar uma funcionalidade que seja capaz de escolher a serie e salvar as informaçoes do aluno em um arquivo .txt

--- modulos.py
+++ modulos.py
@@ -1,16 +1,60 @@
-com=0
+def linha():
+    print('='*40)
+
+def leia_data(texto):
+    while True:
+        inform=leiaint(texto)
+        if inform<31:
+            return inform
+        else:
+            print('Não existe mais do que 31 dias no mês!')
+            linha()
+def leia_mes(texto):
+    while True:
+        mes=leiaint(texto)
+        if mes<=12:
+            return mes
+        else:
+            print('Não existe mais do que 12 meses! ')
+            linha()
+def leia_ano(texto):
+    from datetime import date
+    while True:
+        inform=leiaint(texto)
+        if inform>date.today().year or inform<1930:
+            
+            print('Impossivel ter nascido nesse ano!')
+            linha()
+        else:
+            return inform
+
+
+
+
+def leia_opção(text):
+    while True:
+        numero=leiaint(text)
+        
+        if numero!= 0 and numero!= 1:
+            print('Digite apenas 0 ou 1')
+            linha()
+        else:
+            return numero
+
 def tabela(valor):
-    print(valor)
-    global com
-    com+=1
     for turno in valor:
         for nomedasala,sala in turno.items():
             if not sala == []:
-                print(f'{nomedasala:^39}')
+                print(f'{"Sala":>14}{nomedasala:>1}')
+                linha()
             for lista in sala:
+                if lista['Turno']==0:
+                    lista['Turno']='Manhã'
+                else:
+                    lista['Turno']='Tarde'
                 for keys, aluno in lista.items():
                     print(f'{keys:<25}{aluno}')
-                print('='*56)
+                    linha()
 
 
 
@@ -20,30 +64,29 @@ def leiaint(texto):
         if numero.isnumeric():
             return int(numero)
         else:
+            linha()
             print('NÃO É NUMERICO! DIGITE NOVAMENTE')
- 
 
 def separador_de_salas(valores):
-    print(valores)
     vagas_por_turno=10
     contador=0
     conttarde=5
-    controle_de_manha=0
-    controle_de_tarde=0
+    capacidade_de_alunos_da_sala_manhã=0
+    capacidade_de_alunos_da_sala_tarde=0
     manha={'2000':[],'2001':[],'2002':[],'2003':[],'2004':[]}
     tarde={'2005':[],'2006':[],'2007':[],'2008':[],'200':[]}
     for values in valores:
-       if controle_de_manha<=vagas_por_turno and values['turno']==0:
+       if capacidade_de_alunos_da_sala_manhã<=vagas_por_turno and values['Turno']==0:
             manha[f'200{contador}'].append(values.copy())
-            controle_de_manha+=1
-            if controle_de_manha==2:
+            capacidade_de_alunos_da_sala_manhã+=1
+            if capacidade_de_alunos_da_sala_manhã==2:
                 contador+=1
-                controle_de_manha=0
+                capacidade_de_alunos_da_sala_manhã=0
 
-       if controle_de_tarde<=vagas_por_turno and values['turno']==1:
+       if capacidade_de_alunos_da_sala_tarde<=vagas_por_turno and values['Turno']==1:
             tarde[f'200{conttarde}'].append(values.copy())
-            if controle_de_tarde==2:
-                controle_de_tarde=0
+            if capacidade_de_alunos_da_sala_tarde==2:
+                capacidade_de_alunos_da_sala_tarde=0
                 conttarde+=1
     return manha,tarde
 
--- programa principal.py
+++ programa principal.py
@@ -1,17 +1,19 @@
-from modulos import leiaint,tabela,separador_de_salas
+from modulos import leiaint,tabela,separador_de_salas,leia_opção,leia_data,leia_mes,leia_ano,linha
 
 cont=0
 infomações={}
 tudo=[]
 while True:
     cont+=1
+    linha()
     infomações['aluno']=str(input('Nome do aluno:')).strip()
-    infomações['matricula']=leiaint('Número da matricula:')
-    infomações['data_de_nascimento']=leiaint('Data de nascimento:')
-    infomações['mes_de_nascimento']=leiaint('Mês de nascimento:')
-    infomações['ano_de_nascimento']=leiaint('Ano de nascimento:')
-    infomações['turno']=leiaint('Manha ou tarde(0 para manhã e 1 para de tarde):')
-    escolha2=leiaint('1 para sair do programa 0 para continuar:')
+    infomações['Matricula']=leiaint('Número da matricula:')
+    infomações['data_de_nascimento']=leia_data('Data de nascimento:')
+    infomações['mes_de_nascimento']=leia_mes('Mês de nascimento:')
+    infomações['ano_de_nascimento']=leia_ano('ano de nascimento')
+    infomações['Turno']=leia_opção('Manha ou tarde(0 para manhã e 1 para de tarde):')
+    escolha2=leia_opção('1 para sair do programa 0 para continuar:')
+    linha()
     tudo.append(infomações.copy())
     
     if escolha2 == 1:
